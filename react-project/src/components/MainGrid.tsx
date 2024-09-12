e23745400df034ac7b63718936bd51cd687b751c
sdragonpt <sergionunoribeiro@gmail.com>
2024-09-12 18:48:22 +0100

    update try

--- react-project/src/components/MainGrid.tsx
+++ react-project/src/components/MainGrid.tsx
@@ -12,25 +12,7 @@ const games = [
     { id: 9, name: 'Hades', platforms: ['PC', 'Switch'], image: '/images/hades.jpg' },
     { id: 10, name: 'Overwatch', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/overwatch.png' },
     { id: 11, name: 'The Legend of Zelda: Breath of the Wild', platforms: ['Switch'], image: '/images/zelda.jpg' },
-    { id: 12, name: 'God of War', platforms: ['PS4', 'PS5'], image: '/images/godofwar.png' },
-    { id: 13, name: 'Bloodborne', platforms: ['PS4'], image: '/images/bloodborne.jpg' },
-    { id: 14, name: 'Sekiro: Shadows Die Twice', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/sekiro.jpg' },
-    { id: 15, name: 'Fortnite', platforms: ['PC', 'PS4', 'Xbox', 'Switch'], image: '/images/fortnite.jpg' },
-    { id: 16, name: 'Apex Legends', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/apexlegends.jpg' },
-    { id: 17, name: 'Call of Duty: Warzone', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/warzone.jpg' },
-    { id: 18, name: 'Valorant', platforms: ['PC'], image: '/images/valorant.jpg' },
-    { id: 19, name: 'Genshin Impact', platforms: ['PC', 'PS4', 'Switch'], image: '/images/genshinimpact.jpg' },
-    { id: 20, name: 'Hollow Knight', platforms: ['PC', 'PS4', 'Switch'], image: '/images/hollowknight.jpg' },
-    { id: 21, name: 'Stardew Valley', platforms: ['PC', 'PS4', 'Xbox', 'Switch'], image: '/images/stardewvalley.jpg' },
-    { id: 22, name: 'Celeste', platforms: ['PC', 'PS4', 'Switch'], image: '/images/celeste.jpg' },
-    { id: 23, name: 'Undertale', platforms: ['PC', 'PS4', 'Switch'], image: '/images/undertale.jpg' },
-    { id: 24, name: 'Journey', platforms: ['PS4', 'PC'], image: '/images/journey.jpg' },
-    { id: 25, name: 'Control', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/control.jpg' },
-    { id: 26, name: 'Subnautica', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/subnautica.jpg' },
-    { id: 27, name: 'Cuphead', platforms: ['PC', 'PS4', 'Xbox', 'Switch'], image: '/images/cuphead.jpg' },
-    { id: 28, name: 'No Man\'s Sky', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/nomanssky.jpg' },
-    { id: 29, name: 'Divinity: Original Sin 2', platforms: ['PC', 'PS4', 'Xbox', 'Switch'], image: '/images/divinity.jpg' },
-    { id: 30, name: 'The Outer Worlds', platforms: ['PC', 'PS4', 'Xbox'], image: '/images/theouterworlds.jpg' }
+    { id: 12, name: 'God of War', platforms: ['PS4', 'PS5'], image: '/images/godofwar.png' }
   ];
   
 
